176efd0a55bb5550bef43e5554dfcca0acbad7cf
kevinw123 <kevinwong_895@hotmail.com>
2016-12-21 15:43:13 -0800

    adding css for shoutout box

--- index.php
+++ index.php
@@ -12,19 +12,19 @@
 			</header>
 			<div id="shouts">
 				<ul>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
-					<li class="shout"><span>10:15Pm - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
+					<li class="shout"><span>10:15PM - </span>Brad: Hey what are you guys up to? </li>
 				</ul>
 			</div>
 			<div id="input">
 				<form method="post" action="process.php">
 					<input type="text" name="user" placeholder="Enter Your Name" />
-					<input type="text" name="message" placeholder="ENter A Message"/>
-					<br>
+					<input type="text" name="message" placeholder="Enter A Message"/>
+					<br/>
 					<input class="shout-btn" type="submit" name="submit" value="Shout It Out"/>
 				</form>
 			</div>
