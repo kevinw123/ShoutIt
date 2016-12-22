be7832c25f5630552da38d2476c2d6ada67b9037
kevinw123 <kevinwong_895@hotmail.com>
2016-12-21 18:35:28 -0800

    Adding messages to database and return error message

--- index.php
+++ index.php
@@ -24,6 +24,9 @@
 				</ul>
 			</div>
 			<div id="input">
+				<?php if(isset($_GET['error'])) : ?>
+					<div class="error"><?php echo $_GET['error']; ?></div>
+				<?php endif; ?>
 				<form method="post" action="process.php">
 					<input type="text" name="user" placeholder="Enter Your Name" />
 					<input type="text" name="message" placeholder="Enter A Message"/>
